ae52db961a1920147717f0c319deeb81a8931302
akmanil <maharanaanil424@gmail.com>
2024-12-15 18:23:57 +0530

    Adding aget request which render a html file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-var express = require("express");
-var app = express();
-var port = 3000;
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 3000;
 
 app.get('/' , (req ,res) =>{
     res.send("Hello World!");
+    // res.sendFile(path.join(__dirname ,"index.html"))
 });
-// app.sendFile()
+
 app.get('/about' ,(req ,res) =>{
     res.send('About');
 })
