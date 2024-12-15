8a1d23e10d30205fe84fa4fd4d45fb9b86b31ba8
akmanil <maharanaanil424@gmail.com>
2024-12-15 19:40:01 +0530

    Adding the static file to the express app

--- one.js
+++ one.js
@@ -3,6 +3,10 @@ const path = require("path");
 const app = express();
 const port = 3000;
 
+//using middleware
+
+app.use(express.static(path.join(__dirname ,"public")));
+
 app.get('/' , (req ,res) =>{
     // res.send("Hello World!");
     res.sendFile(path.join(__dirname ,"index.html"))
